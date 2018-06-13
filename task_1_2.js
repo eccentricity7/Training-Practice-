cff1be813f1fda8cca7919b8a09227763f90e90f
Yuliya.Yemelyanovich <e-juliae@mail.ru>
2018-06-13 20:57:58 +0300

    task 2 topic 1 completed

---
+++ task_1_2.js
@@ -0,0 +1,23 @@
+function isPrime(numb) {
+    if(numb < 0)
+    {
+        numb = -numb;
+    }
+    if(numb <= 1 || (numb - Math.round(numb)) != 0)
+    {
+        return false;
+    }
+    if(numb % 2 === 0 || numb % 3 === 0)
+    {
+        return false;
+    }
+    for(var i = 5; i*i < numb; i++) {
+        if(numb % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var numb = prompt("Введите число", "10")
+alert(isPrime(numb))
